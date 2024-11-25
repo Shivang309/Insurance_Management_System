900199deabc200554b54f80310a9225ef056d32f
Shivang309 <115680892+Shivang309@users.noreply.github.com>
2024-11-25 13:29:48 +0530

    Delete Online Insurance Management/Insurance Management directory

--- Online Insurance Management/Insurance Management/Insurance_Management/Insurance/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class InsuranceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Insurance'
-
-    def ready(self):
-        import Insurance.signals
